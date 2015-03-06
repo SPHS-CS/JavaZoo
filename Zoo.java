a6bfcc4595d12abb5cea520f578c2fd780e4a98c
Mr. Shorr <gshorr@spusd.net>
2015-03-06 08:35:23 -0800

    Initially Uploading Base Files
    Zoo main class

---
+++ Zoo.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * Write a description of class Zoo here.
+ * 
+ * @author APCSA2014-15 
+ * @version 2015-03-06
+ */
+public class Zoo
+{
+    public static  void main(String[] args) throws InterruptedException 
+    {
+        List<Random> animals = new ArrayList<Random>();
+
+        System.out.println("Welcome to the Zoo!\n");
+        System.out.print("Building the cages");
+        delayDots();
+        System.out.print("Populating the animals");
+        populateAnimals(animals);
+        delayDots();
+        System.out.print("Hiring zookeepers");
+        delayDots();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nYou are standing in a wondrous zoo. What would you like to do?");
+        System.out.println("Type help to find out what you can do.\n");
+        String text = in.nextLine();
+        String msg = "";
+        while(!text.equals("leave"))
+        {
+            switch(text)
+            {
+                case "help" : msg = "So far we can only leave and ask for help.";
+                                break;
+                default : msg = "You flail helplessly with indecision.";
+            }
+            System.out.println("\n" + msg);
+            System.out.println("\nYou are standing in a wondrous zoo. What would you like to do?\n");
+            text = in.nextLine();
+        }
+        
+        System.out.println(Math.random() > .8 ? "\nHave a nice day!  Hope you come back!" : "\nAn escaped lion eats you on your way out.  Sorry!");
+
+    }
+
+
+    /**
+     * This prints an ellipses with 1 second between each period
+     * It then moves to the next line.
+     */
+    public static void delayDots() throws InterruptedException 
+    {
+        TimeUnit.SECONDS.sleep(1);
+        System.out.print(".");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.print(".");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(".");
+    }
+
+    /**
+     * This is where we will all collaborate.
+     * Construct your animal and add it to the List
+     * @param animals the list containing all the zoo animals
+     */
+    public static void populateAnimals(List<Random> animals)
+    {
+        Random r = new Random();
+        Random q = new Random();
+        animals.add(r);
+        animals.add(q);
+    }
+}
+
