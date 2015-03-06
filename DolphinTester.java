17346903ed1030eff0c300639f46b6bead1a7fb4
Mr. Shorr <gshorr@spusd.net>
2015-03-06 11:40:52 -0800

    Added SWRD subclass to Dolphin

---
+++ DolphinTester.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class DolphinTester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DolphinTester
+{
+    public static void main(String[] args)
+    {
+        Dolphin bobDylan = new Dolphin();
+        System.out.println(bobDylan.play());
+        System.out.println(bobDylan.makeNoise());
+        System.out.println(bobDylan.swim());
+        
+        Dolphin tyroneSWRD = new SeaWorldRescueDolphin();
+        System.out.println(tyroneSWRD.play());
+        System.out.println(tyroneSWRD.makeNoise());
+        System.out.println(tyroneSWRD.swim());
+    }
+}
+
---
+++ SeaWorldRescueDolphin.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class SeaWorldRescueDolphin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SeaWorldRescueDolphin extends Dolphin
+{
+    
+
+    /**
+     * Constructor for objects of class SeaWorldRescueDolphin
+     */
+    public SeaWorldRescueDolphin()
+    {
+      
+    }
+
+    @Override
+    public String play()
+    {
+        return "The dolphin does forty backflips, shooting lasers continuously making a spiral of destruction.";
+    }
+    
+}
+
+
+
+
+
+
+
+
