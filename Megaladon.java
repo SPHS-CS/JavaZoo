0e70c3242dd2adae8fc67dfce6ea8e9bb47dbf10
wasteMangement <382194@CompSci15.local>
2015-03-19 11:29:40 -0700

    Megaladon Class
    A unpractical prehistoric shark that is terrifying to say the
    least.

---
+++ Megaladon.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Megaladon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Megaladon extends Animal implements Swimmable
+{
+    public String eat()
+    {
+        return "The Megaladon devours a small boat";
+    }
+
+    public String makeNoise()
+    {
+        return "The Megaladon roars shrekingly"; 
+    }
+
+    public String move()
+    {
+        return "Charges through the dense water";
+    }
+
+    public String play()
+    {
+        return "The Megaladon taunts it prey";
+
+    }
+    
+    public String swim()
+    {
+        return "The Megaladon swims through the water with a disturbing smile on its face";
+
+    }
+    
+
+}
