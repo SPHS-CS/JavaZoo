25a120e1e0096adc2e37e0f0150366115210ce62
Mr. Shorr <gshorr@spusd.net>
2015-03-19 09:01:54 -0700

    added interfaces

---
+++ Roamable.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of interface Roamable here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Roamable
+{
+    String roam();
+}
