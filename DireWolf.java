7a8639a22f7f7fc3902f458b514dd5871924aa53
JacobLeySan <381952@CompSci18.local>
2015-03-19 14:57:59 -0700

    Direwolf
    In honor of Jon Snow and the North

---
+++ DireWolf.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class DireWolf here.
+ * 
+ * @author (Jacob Ley-Sanchez) 
+ * @version (a version number or a date)
+ */
+public class DireWolf extends Animal implements Roamable
+{
+    public String move()
+    {
+        return "The Direwolf struts forward, growling menacingly.";
+    }
+    public String makeNoise()
+    {
+        return "It releases a loud menacing howl in the defence of the North.";
+    }
+    public String eat()
+    {
+        return "The direwolf feasts on the bones of Lannisters.";
+    }
+    public String play()
+    {
+        return "Direwolves do not play, they hunt.";
+    }
+    public String roam()
+    {
+        return "The direwolf stalks its prey, slowly and calculatingly.";
+    }
+}
