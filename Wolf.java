021bf081f65e6361b5ea650de94369327e7ed34e
JohnS <327798@CompSci09.local>
2015-03-19 11:36:26 -0700

    Created Wolf and LoneWolf classes

---
+++ Wolf.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Wolf here.
+ * 
+ * @author (John Scott) 
+ * @version (2015-03-19)
+ */
+public class Wolf extends Animal implements Roamable
+{
+    public String play()
+    {
+        return "The wolf runs about and does many backflips.";
+    }
+    public String eat()
+    {
+        return "The wolf eats a nearby animal.";
+    }
+    public String sleep()
+    {
+        return "The wolf falls over and sleeps.";
+    }
+    public String makeNoise()
+    {
+        return "Ahh-wwwooooooooo";
+    }
+    public String roam()
+    {
+        return "The wolf walks around looking for potential prey.";
+    }
+    public String move()
+    {
+        return "The wolf moves.";
+    }
+}
