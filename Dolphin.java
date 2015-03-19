67104992deca31ec657dcd34b06d625468b2a553
JordanXiao <382008@CompSci32.local>
2015-03-19 09:03:19 -0700

    Deleted animal tester class?

--- Dolphin.java
+++ Dolphin.java
@@ -7,37 +7,50 @@
  */
 public class Dolphin extends Animal
 {
-    // instance variables - replace the example below with your own 
+    // instance variables - replace the example below with your own
+    private String description;
     private boolean hasBeachBall;
+    private String name;
 
     /**
      * Constructor for objects of class Dolphin
      */
-    public Dolphin(String description, String name, boolean hasBall)
+    public Dolphin()
     {
-        super(description, name);
-        hasBeachBall = hasBall;
+        name = "DolphinRashandala";
+        hasBeachBall = true;
+        description = "This is a pink amazon dolphin.  Much cool.";
     }
 
+    @Override
     public String move()
     {
         return "The dolphin swims about all dolphin-like.";
     }
     
+    @Override
     public String play()
     {
         if(hasBeachBall)
             return "The dolphin plays with the beachball and is having a splendid day.";
         return "The dolphin is depressed at his severe lack of beachball.  He shoots lasers.";
     }
-    public String eat()
-    {
-        return "mmm, fish";
-    }
+    
+    @Override
     public String makeNoise()
     {
         return "EEEEeeeeEEEEeeeeEEEEeeeeEEEEeeeeEEEEeeeeEEEEeeee";
     }
     
+    @Override
+    public String eat()
+    {
+        return "The dolphin leaps out of the water and eats a bird.";
+    }
     
+    @Override
+    public String sleep()
+    {
+        return "The dolphin doesn't sleep because it's afraid to drown.";
+    }
 }
--- SeaWorldRescueDolphin.java
+++ SeaWorldRescueDolphin.java
@@ -12,19 +12,15 @@ public class SeaWorldRescueDolphin extends Dolphin
     /**
      * Constructor for objects of class SeaWorldRescueDolphin
      */
-    public SeaWorldRescueDolphin(String description, String name, boolean hasBall)
+    public SeaWorldRescueDolphin()
     {
-            super(description, name, hasBall);
+      
     }
 
     @Override
     public String play()
     {
-        if(Math.random() < 0.5)
-            return super.play();
-        else      
-            return "The dolphin does forty backflips, shooting lasers continuously making a spiral of destruction.";
-  
+        return "The dolphin does forty backflips, shooting lasers continuously making a spiral of destruction.";
     }
     
 }
