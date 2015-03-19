438d622b6987f7cb1588d700e73faeafb27dd5af
alexkim0418 <319287@CompSci02.local>
2015-03-19 09:29:18 -0700

    Revert c859a2c..4b7e963
    This rolls back to commit c859a2c1bc01c9636619eb1204f39f785bc58d30.

---
+++ SaviorThePhoenix.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * Write a description of class asdf here.
+ * 
+ * @author Alex Kim
+ * @version 3-19-15
+ */
+public class SaviorThePhoenix extends Phoenix
+{
+    private String description;
+    private List<String> toys;
+    /**
+     * Constructor for objects of class CircusBear
+     */
+    public SaviorThePhoenix(String desc, String name)
+    {
+        super(desc, name);
+        toys = new ArrayList<String>();
+        toys.add("Fire");
+        toys.add("Starcraft");
+    }
+
+    @Override
+    public String play()
+    {
+         return "sAviOr is the king of Starcraft matchfixing, nicknamed the phoenix.";
+    }
+}
